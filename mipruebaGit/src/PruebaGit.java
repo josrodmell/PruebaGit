b5565cd9bd9071913a600922c3bb07c504d872e1
nose <nose@192.168.1.251>
2013-04-02 20:59:43 +0200

    borrado propiedades

--- mipruebaGit/src/PruebaGit.java
+++ mipruebaGit/src/PruebaGit.java
@@ -5,7 +5,7 @@ public class PruebaGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Subversion, tú antes eras guay");
 
 	}
 
